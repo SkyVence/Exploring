2a444940e4dd6cf7470644c9cf0def6706850409
SkyVence <68406889+SkyVence@users.noreply.github.com>
2022-04-11 00:11:10 +0200

    test

--- config.php
+++ config.php
@@ -6,6 +6,8 @@
            $bdd = new PDO("mysql:host=CHANGER_HOST_ICI;dbname=CHANGER_DB_NAME;charset=utf8;port=3306", "CHANGER_LOGIN", "CHANGER_PASS");
            
          */
+        ini_set('mysql.connect_timeout,300');
+        ini_set('default_socket_timeout,300');
     try 
     {
         $bdd = new PDO("mysql:host=192.168.4.100;dbname=exploring;charset=utf8;port=3306", "root", "Antoine06,!");
@@ -14,3 +16,4 @@
     {
         die('Erreur : '.$e->getMessage());
     }
+    ?>
