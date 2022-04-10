cf57863fbabb8c6180635d5d046c3c72fa37e6e6
SkyVence <68406889+SkyVence@users.noreply.github.com>
2022-04-11 00:00:53 +0200

    rechange

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
-<?php
-// Database settings
-$db="exploring";
-$dbhost="192.168.4.100";
-$dbport=3307;
-$dbuser="root";
-$dbpasswd="Antoine06,!";
- 
-$bdd = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-$bdd->exec("SET CHARACTER SET utf8");
- 
-?>
+<?php 
+        /*
+           Attention ! le host => l'adresse de la base de données et non du site !!
+        
+           Pour ceux qui doivent spécifier le port ex : 
+           $bdd = new PDO("mysql:host=CHANGER_HOST_ICI;dbname=CHANGER_DB_NAME;charset=utf8;port=3306", "CHANGER_LOGIN", "CHANGER_PASS");
+           
+         */
+    try 
+    {
+        $bdd = new PDO("mysql:host=192.168.4.100;dbname=exploring;charset=utf8;port=3307", "root", "Antoine06,!");
+    }
     catch(PDOException $e)
     {
         die('Erreur : '.$e->getMessage());
